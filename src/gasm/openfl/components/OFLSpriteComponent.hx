3a34197c07eb72a7fd5ed608ae5a028949a6bc67
Leo Bergman <leo@pandacreative.se>
2017-11-21 09:48:54 +0100

    Fix interaction events

--- src/gasm/openfl/components/OFLSpriteComponent.hx
+++ src/gasm/openfl/components/OFLSpriteComponent.hx
@@ -1,7 +1,9 @@
 package gasm.openfl.components;
-import gasm.core.components.SpriteModelComponent;
+import gasm.core.enums.InteractionType;
 import gasm.core.events.InteractionEvent;
+import openfl.display.DisplayObject;
 import openfl.display.DisplayObjectContainer;
+import gasm.core.components.SpriteModelComponent;
 import openfl.display.Sprite;
 import gasm.core.Component;
 import gasm.core.enums.ComponentType;
@@ -14,57 +16,56 @@ import openfl.events.MouseEvent;
 class OFLSpriteComponent extends Component
 {
 	public var sprite(default, default):DisplayObjectContainer;
-	private var _base:Bool;
-	
-	public function new(sprite:Sprite, ?mouseEnabled:Bool = false, base:Bool = false) 
-	{
-		this.sprite = sprite;
-		sprite.mouseEnabled = mouseEnabled;
+	var _base:Bool;
+
+	public function new(sprite:DisplayObject, ?mouseEnabled:Bool = false, base:Bool = true) {
+		if(!Std.is(sprite, DisplayObjectContainer)) {
+			this.sprite = new Sprite();
+			this.sprite.addChild(sprite);
+		} else {
+			this.sprite = cast(sprite, DisplayObjectContainer);
+		}
+		this.sprite.mouseEnabled = mouseEnabled;
 		componentType = ComponentType.Graphics;
 		_base = base;
 	}
-	
-	override public function init()
-	{
+	override public function setup() {
 		var model = owner.get(SpriteModelComponent);
-		if (sprite.width > 0)
-		{
-			model.width = sprite.width;			
+		if (sprite.width > 0) {
+			model.width = sprite.width;
 			model.height = sprite.height;
 		}
-		sprite.addEventListener(MouseEvent.CLICK, function(e:MouseEvent)
-		{
-			
-			/*
-			if(model.pressHandlers != null)
-			{
-				model.pressHandlers(new InteractionEvent( { x:sprite.mouseX, y:sprite.mouseY }, owner));
-			}*/
+	}
+
+	override public function init() {
+		var model = owner.get(SpriteModelComponent);
+		var mask:DisplayObject = model.mask;
+		if(mask != null) {
+			sprite.addChild(mask);
+			sprite.mask = mask;
+		}
+		sprite.addEventListener(MouseEvent.CLICK, function(e:MouseEvent) {
+			model.triggerEvent(InteractionType.PRESS, { x:sprite.mouseX, y:sprite.mouseY }, owner);
 		});
-		sprite.addEventListener(MouseEvent.MOUSE_OVER, function(e:MouseEvent)
-		{
-			/*if(model.overHandler != null)
-			{
-				model.overHandler(new InteractionEvent( { x:sprite.mouseX, y:sprite.mouseY }, owner));
-			}*/
+		sprite.addEventListener(MouseEvent.MOUSE_OVER, function(e:MouseEvent) {
+			model.triggerEvent(InteractionType.OVER, { x:sprite.mouseX, y:sprite.mouseY }, owner);
+
+		});
+		sprite.addEventListener(MouseEvent.MOUSE_MOVE, function(e:MouseEvent) {
+			model.triggerEvent(InteractionType.MOVE, { x:sprite.mouseX, y:sprite.mouseY }, owner);
 		});
 	}
-	
-	override public function update(dt:Float) 
-	{
+
+	override public function update(dt:Float) {
 		var model = owner.get(SpriteModelComponent);
 		sprite.x = model.x + model.offsetX;
 		sprite.y = model.y + model.offsetY;
-		if (_base)
-		{
-			if (sprite.width > 0)
-			{
+		if (_base) {
+			if (sprite.width > 0) {
 				model.width = sprite.width;
-				model.height = sprite.height;				
+				model.height = sprite.height;
 			}
-		}
-		else
-		{
+		} else {
 			sprite.width = model.width;
 			sprite.height = model.height;
 		}
